343ef1fcfacabea0aa9bda5a265db2fa52831c9b
opensmarty <opensmarty@163.com>
2017-09-24 13:11:37 +0800

    ok, this is empty info for committing.

--- application/install/config.php
+++ application/install/config.php
@@ -15,11 +15,7 @@
 return array(
     //产品配置
     'install_product_name'   => 'os_', //产品名称
-<<<<<<< HEAD
     'install_website_domain' => 'http://opensmarty.github.io', //官方网址
-=======
-    'install_website_domain' => 'http://www.baiyf.com/snake', //官方网址
->>>>>>> 4abd2eb0b10c31c1867fff5d171f96d947edba89
     'install_company_name'   => 'opensmarty通用后台', //公司名称
     'original_table_prefix'  => 'os_', //默认表前缀
 
